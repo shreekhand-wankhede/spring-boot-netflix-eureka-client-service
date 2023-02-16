8e6c8cd77add15231498f053b87e1244d978cbcf
shreekhand.wankhede <shreekhand.wankhede@gmail.com>
2023-02-16 23:42:12 +0530

    initial commit

---
+++ src/main/java/com/example/demo/SpringBootNetflixEurekaClientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNetflixEurekaClientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootNetflixEurekaClientServiceApplication.class, args);
+	}
+
+}
